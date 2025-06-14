804d5930a591790eeed24f129351a5fcc078449d
Bernado Diniz <bernado.felix@estudante.ufla.br>
2025-06-13 23:31:03 -0300

    Implementação do CRUD de usuários

---
+++ backend/src/user/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'O nome não pode ser vazio.' })
+  nome: string;
+
+  @IsEmail({}, { message: 'Forneça um endereço de e-mail válido.' })
+  email: string;
+
+  @IsNotEmpty({ message: 'A senha não pode ser vazia.' })
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres.' })
+  senha: string;
+}
